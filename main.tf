ceae06a1521b0fe82639d8bace060e8fb59a3efc
g3sporter <52547590+g3sporter@users.noreply.github.com>
2019-12-20 14:18:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,42 @@
+# define a variable called instance_count but dont provide a default so that user will be prompted to provide
+variable "region" {
+    default = "us-east-2"
+    }
+variable "instance_count" { }
+variable "requester" {
+  default = "g3sporter"
+}
+
+
 variable "size" {
     description = "size of instances"
-    default = {"name_description"="medium_ones","amount"="4"}
+    default = {"name_description"="medium_ones","amount"="4","instance_type"="t2.micro}
 }
 
-module "ec2_cluster" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "~> 2.0"
-
-  name                   = var.size.name_description
-  instance_count         = var.size.amount
-
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "azc"
-  subnet_id              = element(module.vpc.private_subnets,0)
-  
+# Use the AWS provider and deploy resources in us-east-2 region
+provider "aws" {
+  region = ${var.region"}
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
 
-  name = "azc-vpc-test"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-2a"]
-  private_subnets = ["10.0.1.0/24"]
-  
-  
+# create a number of EC2 instances
+resource "aws_instance" "my_instance" {
+  # if var.instance count is > 2 ? then create 0 instances : else create the number specified in var.instance_count
+  count = var.size.amount < 10 ? 0 : var.size.amount
+  # The amazon machine image number (only valid in us-east-2)
+  ami = "ami-0d8f6eb4f641ef691"
+  # The instance size
+ instance_type = var.size.instance_type
+  #instance_type = "t2.micro"
+  # Specify this block of tags on the resource
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    # set the tag key: "Name"  = "g3sporter-1". Adding 1 to count.index because it starts at zero
+    Name = "${var.requester}-${count.index + 1}"
   }
+}
+
+# Create an output with instance name and IP address
+output "instance_ids" {
+  # create a list of private_ip addresses of each instance
+  value = aws_instance.my_instance[*].private_ip
 }
