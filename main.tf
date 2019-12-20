b200d358d1f964cde269212dba25413c7d6c0512
g3sporter <52547590+g3sporter@users.noreply.github.com>
2019-12-20 14:25:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "requester" {
 
 variable "size" {
     description = "size of instances"
-    default = {"name_description"="medium_ones","amount"="4","instance_type"="t2.micro"}
+    default = {"name_description"="medium_ones","instance_type"="t2.micro"}
 }
 
 # Use the AWS provider and deploy resources in us-east-2 region
@@ -22,7 +22,7 @@ provider "aws" {
 # create a number of EC2 instances
 resource "aws_instance" "my_instance" {
   # if var.instance count is > 2 ? then create 0 instances : else create the number specified in var.instance_count
-  count = var.size.amount < 10 ? 0 : var.size.amount
+  count = var.instance_count < 10 ? 0 : var.instance_count
   # The amazon machine image number (only valid in us-east-2)
   ami = "ami-0d8f6eb4f641ef691"
   # The instance size
