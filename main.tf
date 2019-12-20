2b99198da8d0bb8d311c59c1cac7cecbc1886675
g3sporter <52547590+g3sporter@users.noreply.github.com>
2019-12-20 15:12:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,14 @@ variable "requester" {
   default = "g3sporter"
 }
 
+name_description   = var.size == "small" ? "small-one" : "medium_one"
+instance_type   = var.size == "small" ? "t2.micro" : "t2.small"
 
-variable "size" {
-    description = "size of instances"
-    default = {"name_description"="medium_ones","instance_type"="t2.micro"}
-    small = {"name_description"="small_ones","instance_type"="t2.small"
-}
+#variable "size" {
+#    description = "size of instances"
+#    default = {"name_description"="medium_ones","instance_type"="t2.micro"}
+#    small = {"name_description"="small_ones","instance_type"="t2.small"
+#}
 
 # Use the AWS provider and deploy resources in us-east-2 region
 provider "aws" {
@@ -26,7 +28,7 @@ resource "aws_instance" "my_instance" {
   # The amazon machine image number (only valid in us-east-2)
   ami = "ami-0d8f6eb4f641ef691"
   # The instance size
- instance_type = var.size.instance_type
+ instance_type = var.instance_type
   #instance_type = "t2.micro"
   # Specify this block of tags on the resource
   tags = {
